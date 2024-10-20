5a06370e424558c69bbead62722ae105002f51cf
moderate301297 <truongluat3012@gmal.com>
2024-10-20 17:19:10 +0700

    fix

--- src/modules/missions/missions.module.ts
+++ src/modules/missions/missions.module.ts
@@ -6,12 +6,14 @@ import { MISSIONS_MODEL, MissionsSchema } from "./schemas/missions.schema";
 import { MissionsController } from "./missions.controller";
 import { USER_MISSIONS_MODEL, UserMissionsSchema } from "./schemas/user-missions.schema";
 import { UsersModule } from "modules/users/users.module";
+import { VotingsModule } from "modules/votings/votings.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: MISSIONS_MODEL, schema: MissionsSchema }]),
     MongooseModule.forFeature([{ name: USER_MISSIONS_MODEL, schema: UserMissionsSchema }]),
     forwardRef(() => UsersModule),
+    VotingsModule
   ],
   providers: [MissionsService],
   controllers: [MissionsController],
